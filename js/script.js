f5a3169c88fdeff52a63b31464623822d6cfeff6
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2021-09-15 20:51:00 +0300

    Add development JS

--- js/script.js
+++ js/script.js
@@ -10,3 +10,16 @@ $(document).ready(function() {
         $("#design-image").slideDown('1500');
     });
 });
+
+// Development Image JS
+
+$(document).ready(function() {
+    $("#development-image").click(function() {
+        $("#development-image").slideDown('1500').hide('1000');
+        $("#development").show('1500');
+    });
+    $("#development").click(function() {
+        $("#development").slideUp('1500');
+        $("#development-image").slideDown('1500');
+    });
+});
